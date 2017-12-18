e7c276f4aa4d79b88afa3bdff4235aa05de55f06
Philip Poten <philip@poten.at>
2017-12-18 10:51:54 +0100

    fix zsh, add install

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -3,5 +3,8 @@
 SCRIPT_DIR=`dirname $0`
 FULL_DIR="`( cd \"$SCRIPT_DIR\" && pwd )`"
 
+test -f /usr/local/bin/brew && brew install tmux
+test -f /usr/bin/apt-get && apt-get install -y tmux
+
 rm -f ~/.tmux.conf
 ln -s $FULL_DIR/tmux.conf ~/.tmux.conf
